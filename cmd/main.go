01b1b546c6577f677c44a7cb93bc86b22de5e2f0
AlagusuryaPeriyakaruppan <alagup1405@gmail.com>
2024-06-08 21:23:46 +0530

    Created Schema to add users to the UserTable

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"context"
+	"fmt"
+	"log"
 
 	db "github.com/AlagusuryaPeriyakaruppan/go-api/internal/database"
 )
@@ -12,5 +12,12 @@ func main() {
 	fmt.Println("Surya loves everyone!")
 	ctx := context.Background()
 	db.InitDB()
-	db.EntClient.User.Create().SetID(29).SetUsername("PP").Save(ctx)
+	if err := db.EntClient.Schema.Create(ctx); err != nil {
+		log.Println("Failed to create schema")
+	}
+	user, err := db.EntClient.User.Create().SetID(29).SetUsername("PP").Save(ctx)
+	if err != nil {
+		log.Println("Error creating User")
+	}
+	fmt.Println(user, "Creted User")
 }
